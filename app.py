d69c07777fbcff1cfce1a53a006f48339a33edf1
Samuel Henrique Oliveira da Silva <samosaara@gmail.com>
2017-11-11 18:50:42 -0200

    Construtor data e conexão banco

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, request
+from cloudant.client import Cloudant
+from os import environ
+from sys import exit
+
 app = Flask(__name__)
+
+try:
+    client = Cloudant(environ.get("USERNAME"), environ.get(
+        "PASSWORD"), url=environ.get("URL"), connect=True)
+    ocorrencia = client['ocorrencia']
+    usuario = client['usuario']
+except Exception as e:
+    if isinstance(e, KeyError):
+        ocorrencia = client.create_database('ocorrencia')
+        usuario = client.create_database('usuario')
+    else:
+        print("Não foi possivel conectar a database")
+        exit(1)
+
 
 '''
 @app.route('/<nome>/<float:idade>')
@@ -16,7 +34,9 @@ def outro(nome, idade):
         pass
     return f"seja bem vindo {nome} {idade} {request.method}"
 '''
-@app.route('/v1/buraco_rua', methods= ["GET", "PUT", "DELETE"])
+
+
+@app.route('/v1/buraco_rua', methods=["GET", "PUT", "DELETE"])
 def buraco():
     if request.method == 'GET':
         pass
@@ -24,15 +44,17 @@ def buraco():
         pass
     elif request.method == 'DELETE':
         pass
-    
+
 
 @app.route('/v1/buraco_rua/deletar')
 def deletar_buraco():
     return "Buraco criado"
 
+
 @app.route('/v1/calcada_quebrada/criar')
 def criar_calcada():
     return "Calcada quebrada adicionada"
+
 
 @app.route('/v1/calcada_quebrada/deletar')
 def deletar_calcada():
--- data.py
+++ data.py
@@ -1,12 +1,14 @@
 from enum import Enum
 
+
 class Ocorrencia:
-    def __init__(self):
-        self.lat: float
-        self.long: float
-        self.date: int
-        self.tipo: Tipo
-    
+    def __init__(self, lat, long, date, tipo):
+        self.lat: float = lat
+        self.long: float = long
+        self.date: int = date
+        self.tipo: Tipo = tipo
+
+
 class Tipo(Enum):
     BURACO = 1
     ENTULHO = 2
